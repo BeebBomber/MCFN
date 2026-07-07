c793d93346b7de8ca909e37b59cf74d85158f90b
BeebBomber <gapsha@gmail.com>
2026-07-07 14:32:42 +0300

    build: add flake.nix for nix run + vendored deps
    Позволяет запустить: nix run github:BeebBomber/MCFN
    Также добавляет devShell с go для разработки: nix develop

--- flake.nix
+++ flake.nix
@@ -1,10 +1,34 @@
 {
-  description = "MCFN Generated System";
+  description = "MCFN — NixOS Config Generator";
+
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.bomb = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forAll = f: nixpkgs.lib.genAttrs systems f;
+    in {
+      packages = forAll (system: {
+        default = nixpkgs.legacyPackages.${system}.buildGoModule {
+          pname = "mcfn";
+          version = "0.2.0";
+          src = ./.;
+          vendorHash = null;
+          meta.mainProgram = "mcfn";
+        };
+      });
+
+      apps = forAll (system: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/mcfn";
+        };
+      });
+
+      devShells = forAll (system: {
+        default = nixpkgs.legacyPackages.${system}.mkShell {
+          buildInputs = [ nixpkgs.legacyPackages.${system}.go ];
+        };
+      });
     };
-  };
 }
